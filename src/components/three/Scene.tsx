221a6271bb81324fee48d71704379646279873fe
CodeWithThree <84151954+Cheung-KaWai@users.noreply.github.com>
2025-04-19 23:04:05 +0200

    chore: restructure files

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -2,7 +2,7 @@ import { Container } from "@components/ui/atoms/Container";
 import { CameraControls, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Splash } from "./waterfall/Splash";
-import { Island } from "./Island";
+import { Island } from "./island/Island";
 import { Waterfall } from "./waterfall/Waterfall";
 import { Rock } from "./waterfall/Rock";
 import { Tree } from "./tree/Tree";
